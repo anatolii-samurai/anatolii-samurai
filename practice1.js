db855d8705831c30a7c9925ba13848671a9d2ba9
Anatolii <goddevil22@yandex.ru>
2023-03-25 11:21:12 +0400

    calc

---
+++ practice1.js
@@ -0,0 +1,13 @@
+function calc(operation,a,b){
+    switch(operation){
+      case "add":
+      return a + b;
+      case "multi":
+      return a * b;
+      case "subctract":
+        return a - b;
+   }
+  }
+  console.log(calc("add",89,2));
+  console.log(calc("multi",5,2));
+  console.log(calc("subctract",8,2));
